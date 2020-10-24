eafd65fe53d3036a333ca5a2e6b591f22b05287d
vivacious-revert-7013 <mesmith454@gmail.com>
2020-10-24 23:13:45 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,9 +1,8 @@
 count = 10 
 new_year = "Happy New Year!"
 
-while count > 1 do
-  puts "#{count}!"
+while count >=1 do
+  puts count "!"
   count -= 1
-  when count = 1 
-    puts "#{new_year}"
-  end
+end
+  puts "#{new_year}"  
